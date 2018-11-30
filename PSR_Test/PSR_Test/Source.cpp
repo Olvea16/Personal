c1ecdd62a3bfbf376cdb7bb355c4cc13c264fd99
Oliver Vea <olvea16@student.sdu.dk>
2018-11-30 08:07:11 +0100

    More data in images.

--- PSR_Test/PSR_Test/Source.cpp
+++ PSR_Test/PSR_Test/Source.cpp
@@ -18,23 +18,30 @@ struct sNearestPoint {
 	Point nearestPoint;
 };
 
+struct IterationData {
+	std::vector<sNearestPoint> nPs;
+	Point centroids[2];
+};
+
 transformation ICP(std::vector<Point> cloudA, std::vector<Point> cloudB, transformation guess = transformation{Point(0,0),0}) {
 	transformation T = guess;
-	std::vector<std::vector<Point>> clouds;
+	std::vector<IterationData> data;
 
 	for (Point& p : cloudA) {
 		p = p.rotate(T.angle);
 		p += T.p;
 	}
-	clouds.push_back(cloudA);
+	std::vector<Point> _cloudA = cloudA;
 
-	int nA = cloudA.size(), nB = cloudB.size();
+	int nA = _cloudA.size(), nB = _cloudA.size();
 	int N = 100;
+
+
 	for (int i = 0; i < N; i++) {
 		Point centroidA = Point(0, 0), centroidB = Point(0, 0);
 		std::vector<sNearestPoint> nPs;
 
-		for (Point pA : cloudA) {
+		for (Point pA : _cloudA) {
 			centroidA += pA;
 
 			Point nearestPoint;
@@ -56,9 +63,9 @@ transformation ICP(std::vector<Point> cloudA, std::vector<Point> cloudB, transfo
 
 		cv::Mat H(2, 2, CV_64F);
 		H = 0;
-		int N = MIN(cloudA.size(), cloudB.size());
+		int N = MIN(_cloudA.size(), _cloudA.size());
 		for (int i = 0; i < N; i++) {
-			Point pA = cloudA[i] - centroidA, pB = cloudB[i] - centroidB;
+			Point pA = _cloudA[i] - centroidA, pB = cloudB[i] - centroidB;
 			double pa[2] = { pA.x(), pA.y() };
 			double pb[2] = { pB.x(), pB.y() };
 
@@ -84,34 +91,52 @@ transformation ICP(std::vector<Point> cloudA, std::vector<Point> cloudB, transfo
 		T.p = T.p.rotate(a);
 		T.p += translation;
 
-		for (Point& p : cloudA) {
+		for (Point& p : _cloudA) {
 			p = p.rotate(a);
 			p += translation;
 		}
 		
-		clouds.push_back(cloudA);
+		IterationData d;
+		d.centroids[0] = centroidA, d.centroids[1] = centroidB;
+		d.nPs = nPs;
+		data.push_back(d);
 	}
 
 	Point min(0,0), max(0,0);
 
-	for (std::vector<Point> c : clouds) {
-		for (Point p : c) {
-			min.x() = MIN(p.x(), min.x());
-			min.y() = MIN(p.y(), min.y());
-			max.x() = MAX(p.x(), max.x());
-			max.y() = MAX(p.y(), max.y());
+	std::vector<Point> points;
+	for (IterationData d : data) {
+		for (int i = 0; i < d.nPs.size(); i++) {
+			points.push_back(d.nPs[i].p);
+			points.push_back(d.nPs[i].nearestPoint);
 		}
 	}
+	for (Point p : cloudA) points.push_back(p);
+	for (Point p : cloudB) points.push_back(p);
+
+
+	for (Point p : points) {
+		min.x() = MIN(p.x(), min.x());
+		min.y() = MIN(p.y(), min.y());
+		max.x() = MAX(p.x(), max.x());
+		max.y() = MAX(p.y(), max.y());
+	}
+	
 
 	Point translation = Point(0,0) - min;
 	Point scale = Point(abs(max.x() - min.x()), abs(max.y() - min.y()));
 
-	for (std::vector<Point>& c : clouds) {
-		for (Point& p : c) {
-			p += translation;
-			p.x() /= scale.x();
-			p.y() /= scale.y();
-		}
+	//for (std::vector<Point>& c : clouds) {
+	//	for (Point& p : c) {
+	//		p += translation;
+	//		p.x() /= scale.x();
+	//		p.y() /= scale.y();
+	//	}
+	//}
+	for (Point& p : cloudA) {
+		p += translation;
+		p.x() /= scale.x();
+		p.y() /= scale.y();
 	}
 
 	for (Point& p : cloudB) {
@@ -121,12 +146,34 @@ transformation ICP(std::vector<Point> cloudA, std::vector<Point> cloudB, transfo
 	}
 
 	int width = 1720, height = 980, r = 3;
-	cv::Vec3b colorA = cv::Vec3b(120, 40, 0), colorB = cv::Vec3b(0, 40, 120);
+	cv::Vec3b colorCloud = cv::Vec3b(120, 40, 0), colorActiveTarget = cv::Vec3b(0, 40, 120), colorPassiveTarget = colorActiveTarget * 0.5, colorLine = cv::Vec3b(60, 60, 60), colorCentroidA = cv::Vec3b(255,0,0), colorCentroidB = cv::Vec3b(0,0,255);
 
-	for (std::vector<Point>& c : clouds) {
+	for (IterationData d : data) {
 		cv::Mat img(cv::Size(width, height), CV_8UC3);
-		for (Point p : cloudB) cv::circle(img, p.getCVPoint(width, height), r, colorA, -1);
-		for (Point& p : c)  cv::circle(img, p.getCVPoint(width, height), r, colorB, -1);
+		for (int i = 0; i < 2; i++) {
+			d.centroids[i] += translation;
+			d.centroids[i].x() *= width / scale.x();
+			d.centroids[i].y() *= height / scale.y();
+		}
+		for (sNearestPoint& np : d.nPs) {
+			np.nearestPoint += translation;
+			np.nearestPoint.x() *= width / scale.x();
+			np.nearestPoint.y() *= height / scale.y();
+
+			np.p += translation;
+			np.p.x() *= width / scale.x();
+			np.p.y() *= height / scale.y();
+		}
+		for (int i = 0; i < d.nPs.size(); i++) cv::line(img, d.nPs[i].nearestPoint.getCVPoint(), d.nPs[i].p.getCVPoint(), colorLine);
+		cv::Rect rectA(d.centroids[0].getCVPoint(), cv::Size(r, r) * 2);
+		cv::rectangle(img, rectA, colorCentroidA, -1);
+		cv::Rect rectB(d.centroids[1].getCVPoint(), cv::Size(r, r) * 2);
+		cv::rectangle(img, rectB, colorCentroidB, -1);
+		for (Point p : cloudB) cv::circle(img, p.getCVPoint(width, height), r, colorPassiveTarget, -1);
+		for (sNearestPoint np : d.nPs) {
+			cv::circle(img, np.nearestPoint.getCVPoint(), r, colorActiveTarget, -1);
+			cv::circle(img, np.p.getCVPoint(), r, colorCloud, -1);
+		}
 		cv::imshow("Image", img);
 		cv::waitKey();
 	}
